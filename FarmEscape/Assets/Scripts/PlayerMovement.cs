3488536350d5390d19f7516431e31ebaa7b0bd0f
GitHub-Bong <bongwl-wm21@student.tarc.edu.my>
2022-09-17 02:47:29 +0800

    hedge jump added

--- FarmEscape/Assets/Scripts/PlayerMovement.cs
+++ FarmEscape/Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,7 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D body;
     [SerializeField] private float speed;
     private Animator anim;
+    private bool grounded;
 
     private void Awake() {
         //Grab references for rigidbody and animator from object
@@ -23,7 +24,7 @@ public class PlayerMovement : MonoBehaviour
     private void Update()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
-        body.velocity = new Vector2(horizontalInput* speed,body.velocity.y);
+        body.velocity = new Vector2(horizontalInput* speed, body.velocity.y);
 
         //flip player when moving left or right
         if (horizontalInput > 0.01f)
@@ -31,10 +32,23 @@ public class PlayerMovement : MonoBehaviour
         else if (horizontalInput < -0.01f)
             transform.localScale = new Vector3(-8, 8, 8);
 
-        if (Input.GetKey(KeyCode.Space))
-            body.velocity = new Vector2(body.velocity.x, speed);
+        if (Input.GetKey(KeyCode.Space) && grounded)
+            Jump();
 
         //Set animator parameters
         anim.SetBool("run", horizontalInput != 0);
+        anim.SetBool("grounded", grounded);
+    }
+
+    private void Jump()
+    {
+        body.velocity = new Vector2(body.velocity.x, speed);
+        anim.SetTrigger("jump"); 
+        grounded = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if(collision.gameObject.tag == "Ground")
+        grounded = true;
     }
 }
