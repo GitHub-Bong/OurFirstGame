1127745207d057d0f43175280c841b88a57aef1f
GitHub-Bong <bweiliet@gmail.com>
2022-09-17 00:26:23 +0800

    animation1 added

--- FarmEscape/Assets/Scripts/PlayerMovement.cs
+++ FarmEscape/Assets/Scripts/PlayerMovement.cs
@@ -4,9 +4,12 @@ public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D body;
     [SerializeField] private float speed;
+    private Animator anim;
 
     private void Awake() {
+        //Grab references for rigidbody and animator from object
         body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
     
     /*
@@ -19,9 +22,19 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed,body.velocity.y);
+        float horizontalInput = Input.GetAxis("Horizontal");
+        body.velocity = new Vector2(horizontalInput* speed,body.velocity.y);
+
+        //flip player when moving left or right
+        if (horizontalInput > 0.01f)
+            transform.localScale = new Vector3(8,8,8);
+        else if (horizontalInput < -0.01f)
+            transform.localScale = new Vector3(-8, 8, 8);
 
         if (Input.GetKey(KeyCode.Space))
             body.velocity = new Vector2(body.velocity.x, speed);
+
+        //Set animator parameters
+        anim.SetBool("run", horizontalInput != 0);
     }
 }
